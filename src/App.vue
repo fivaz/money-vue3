84ca1fe6d6e63f249fcf25147d2e9401cc39f499
fivaz <s.a.fivaz@gmail.com>
2024-06-12 16:17:46 +0200

    add tailwind font

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,6 @@
+<style>
+@import 'https://rsms.me/inter/inter.css';
+</style>
 <template>
 	<RouterView />
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
 
+const defaultTheme = require('tailwindcss/defaultTheme')
+export default {
+	content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+			},
+		},
+	},
+	plugins: [require('@tailwindcss/forms')],
+}
