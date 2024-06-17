a9d54be6a4a8d49bb36d8e0c7cdda2b34ee4b2ce
fivaz <s.a.fivaz@gmail.com>
2024-06-17 23:25:01 +0200

    refactor

--- src/components/NavbarMenu.vue
+++ src/components/NavbarMenu.vue
@@ -44,7 +44,7 @@
 							>
 								<button
 									@click="logout"
-									class="text-sm font-semibold leading-6 text-gray-900 dark:text-white"
+									class="text-sm font-semibold leading-6 text-gray-900 hover:text-white dark:text-white"
 								>
 									Log out <span aria-hidden="true">&rarr;</span>
 								</button>
