3ef0bcafe98481782e31126b6d1a5cebf69e6f4b
fivaz <s.a.fivaz@gmail.com>
2024-06-17 19:31:23 +0200

    isolate code into components

--- src/components/transaction/TransactionForm.vue
+++ src/components/transaction/TransactionForm.vue
@@ -186,14 +186,7 @@
 		</div>
 
 		<div :class="['flex', transaction.id ? 'justify-between' : 'justify-end']">
-			<button
-				v-if="transaction.id"
-				@click="handleDelete"
-				type="button"
-				class="rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-			>
-				Delete
-			</button>
+			<DarkButton2 v-if="transaction.id" @click="handleDelete"> Delete </DarkButton2>
 			<button
 				type="submit"
 				class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
@@ -226,6 +219,7 @@ import { format, getMonth, parseISO, set } from 'date-fns'
 import { DATETIME_OUT } from '@/lib/consts'
 import Toggle from '@/components/form/Toggle.vue'
 import LabelInput from '@/components/form/LabelInput.vue'
+import DarkButton2 from '@/components/button/DarkButton2.vue'
 
 const props = defineProps<{
 	budgets: Budget[]
