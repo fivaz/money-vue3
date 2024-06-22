a3487587453a2b332cb34b9be86b310feae8aec5
fivaz <s.a.fivaz@gmail.com>
2024-06-23 01:22:47 +0200

    fix assets for PWA on iphone

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,16 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue(), VitePWA({ registerType: 'autoUpdate' })],
+	plugins: [
+		vue(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			pwaAssets: {
+				config: true,
+				disabled: false,
+			},
+		}),
+	],
 	resolve: {
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url)),
