e81c2aca707f4f9cb71931679115eb4e9dc1c22a
fivaz <s.a.fivaz@gmail.com>
2024-12-29 19:28:13 +0100

    improve design of account transactions on mobile

--- src/components/account/AccountTransactionItem.vue
+++ src/components/account/AccountTransactionItem.vue
@@ -7,11 +7,15 @@
 			'border-b p-3 hover:underline',
 		]"
 	>
-		<button @click="$emit('edit', transaction)" class="flex w-full justify-between">
-			<span class="flex items-center gap-4">
-				<span class="block w-20 text-left md:hidden">
-					{{ format(transaction.date, SHORT_DATETIME_FR) }}
+		<button
+			@click="$emit('edit', transaction)"
+			class="flex w-full items-center justify-between gap-2 md:gap-5"
+		>
+			<span class="flex flex-shrink-0 gap-2 whitespace-nowrap">
+				<span class="block text-left md:hidden">
+					{{ format(transaction.date, 'dd') }}
 				</span>
+
 				<span class="hidden gap-2 md:flex">
 					<span class="flex w-10 items-center gap-2">
 						<CalendarCheck class="h-4 w-4" />
@@ -22,15 +26,27 @@
 						<span>{{ format(transaction.date, 'HH:mm') }}</span>
 					</span>
 				</span>
-				<span v-if="transaction.description">{{ transaction.description }}</span>
-				<span v-else>{{ transaction.budget?.name }}</span>
+			</span>
+
+			<div class="h-4 w-4 flex-shrink-0">
 				<component
 					:is="getIcon(transaction.budget.icon)"
 					class="h-4 w-4"
 					v-if="transaction.budget"
 				/>
+			</div>
+
+			<span class="flex-grow overflow-hidden text-ellipsis whitespace-nowrap text-left">
+				<span v-if="transaction.description">{{ transaction.description }}</span>
+				<span v-else>{{ transaction.budget?.name }}</span>
 			</span>
-			<span :class="['flex items-center gap-2', getTransactionAmountColor()]">
+
+			<span
+				:class="[
+					'flex flex-shrink-0 items-center gap-2 whitespace-nowrap',
+					getTransactionAmountColor(),
+				]"
+			>
 				<ArrowRightLeft class="h-5 w-5" v-if="transaction.operation === 'transfer'" />
 				{{ formatAmount() }}
 			</span>
@@ -39,12 +55,7 @@
 </template>
 
 <script setup lang="ts">
-import {
-	MAIN_HOVER_COLOR_BG,
-	MAIN_HOVER_COLOR_TEXT,
-	SECONDARY_COLOR_TEXT,
-	SHORT_DATETIME_FR,
-} from '@/lib/consts'
+import { MAIN_HOVER_COLOR_BG, MAIN_HOVER_COLOR_TEXT, SECONDARY_COLOR_TEXT } from '@/lib/consts'
 import { type Transaction, parseAmount } from '@/lib/transaction'
 import { getAmountColor, getIcon } from '@/lib/utils'
 import { format } from 'date-fns'
