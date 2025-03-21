7fa1a5e0f7eff80cf942ab7441124d91ab7e37e9
fivaz <s.a.fivaz@gmail.com>
2025-03-21 04:44:16 +0100

    add disclosure

--- src/components/accounts/account-item.vue
+++ src/components/accounts/account-item.vue
@@ -6,7 +6,7 @@ import NText from '@/components/base/n-text.vue'
 import {
   CogIcon,
   PlusIcon,
-  ChevronUpIcon,
+  ChevronDownIcon,
   ArrowDownNarrowWideIcon,
   ArrowDownWideNarrowIcon,
 } from 'lucide-vue-next'
@@ -17,6 +17,7 @@ import { useTransactionsStore } from '@/components/transactions/store'
 import { useDateStore } from '@/store/date.ts'
 import IconRenderer from '@/components/base/Icon/icon-renderer.vue'
 import NButton from '@/components/base/n-button.vue'
+import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/vue'
 
 const props = defineProps<{
   account: Account
@@ -50,45 +51,58 @@ const sortedTransactions = computed(() =>
 </script>
 
 <template>
-  <li class="rounded-lg border border-gray-300 bg-gray-100 dark:border-gray-600 dark:bg-gray-800">
-    <div
-      class="rounded-x-lg flex items-center justify-between rounded-t-lg border-b border-gray-300 p-3 dark:border-gray-600"
-    >
-      <div class="flex items-center gap-2">
-        <NText>
-          <IconRenderer class="size-5" :name="account.icon" />
-        </NText>
-        <NText size="lg" class="font-medium">{{ account.name }}</NText>
+  <Disclosure defaultOpen v-slot="{ open }">
+    <li class="rounded-lg border border-gray-300 bg-gray-100 dark:border-gray-600 dark:bg-gray-800">
+      <div
+        class="rounded-x-lg flex items-center justify-between rounded-t-lg border-b border-gray-300 p-3 dark:border-gray-600"
+      >
+        <div class="flex items-center gap-2">
+          <NText>
+            <IconRenderer class="size-5" :name="account.icon" />
+          </NText>
+          <NText size="lg" class="font-medium">{{ account.name }}</NText>
+        </div>
+        <div class="flex gap-2">
+          <n-button @click="isAscending = !isAscending">
+            <ArrowDownNarrowWideIcon v-if="isAscending" class="size-4" />
+            <ArrowDownWideNarrowIcon v-else class="size-4" />
+          </n-button>
+          <TransactionForm
+            color="primary"
+            :transaction="newTransaction"
+            :is-account-annual="account.isAnnual"
+          >
+            <PlusIcon class="size-4" />
+          </TransactionForm>
+          <AccountForm :account="account">
+            <CogIcon class="size-4" />
+          </AccountForm>
+        </div>
       </div>
-      <div class="flex gap-2">
-        <n-button @click="isAscending = !isAscending">
-          <ArrowDownNarrowWideIcon v-if="isAscending" class="size-4" />
-          <ArrowDownWideNarrowIcon v-else class="size-4" />
-        </n-button>
-        <TransactionForm
-          color="primary"
-          :transaction="newTransaction"
-          :is-account-annual="account.isAnnual"
-        >
-          <PlusIcon class="size-4" />
-        </TransactionForm>
-        <AccountForm :account="account">
-          <CogIcon class="size-4" />
-        </AccountForm>
-      </div>
-    </div>
 
-    <ul role="list" class="divide-y divide-gray-300 dark:divide-gray-600">
-      <TransactionItem
-        v-for="transaction in sortedTransactions"
-        :key="transaction.id"
-        :transaction="transaction"
-        :account="account"
-      />
-    </ul>
+      <transition
+        enter-active-class="transition duration-100 ease-out"
+        enter-from-class="transform scale-95 opacity-0"
+        enter-to-class="transform scale-100 opacity-100"
+        leave-active-class="transition duration-75 ease-out"
+        leave-from-class="transform scale-100 opacity-100"
+        leave-to-class="transform scale-95 opacity-0"
+      >
+        <DisclosurePanel>
+          <ul role="list" class="divide-y divide-gray-300 dark:divide-gray-600">
+            <TransactionItem
+              v-for="transaction in sortedTransactions"
+              :key="transaction.id"
+              :transaction="transaction"
+              :account="account"
+            />
+          </ul>
+        </DisclosurePanel>
+      </transition>
 
-    <div class="flex justify-center p-2">
-      <n-text><ChevronUpIcon class="size-5" /></n-text>
-    </div>
-  </li>
+      <DisclosureButton class="flex w-full justify-center p-2">
+        <n-text><ChevronDownIcon :class="open && 'rotate-180 transform'" /></n-text>
+      </DisclosureButton>
+    </li>
+  </Disclosure>
 </template>
